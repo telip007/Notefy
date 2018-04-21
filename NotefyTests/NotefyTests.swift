beca414d0d43e491465e14d8c3caa016505f2549
Talip Göksu <talip.goeksu@gmail.com>
2018-04-21 23:04:07 +0200

    Initial Commit

---
+++ NotefyTests/NotefyTests.swift
@@ -0,0 +1,36 @@
+//
+//  NotefyTests.swift
+//  NotefyTests
+//
+//  Created by Talip Göksu on 21.04.18.
+//  Copyright © 2018 Iman Studios. All rights reserved.
+//
+
+import XCTest
+@testable import Notefy
+
+class NotefyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
